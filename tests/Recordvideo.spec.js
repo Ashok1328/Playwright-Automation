445496f648fbee8bdb8abfaae7984871274f28c5
Ashok1328 <ashokkhatiwada1990@gmail.com>
2025-10-02 00:04:14 +0545

    Video Recording in Plawright with automatic handling throghy config.js while running the test application

---
+++ tests/Recordvideo.spec.js
@@ -0,0 +1,16 @@
+import { test, expect } from "@playwright/test";
+import { ECDH } from "crypto";
+
+test("Recording Video", async ({ page }) => {
+  await page.goto("https://www.demoblaze.com/index.html");
+
+  await page.getByRole("link", { name: "Log in" }).click();
+
+  await page.locator("#loginusername").fill("Ashik");
+
+  await page.locator("#loginpassword").fill("Ashik@123");
+
+  await page.getByRole("button", { name: "Log in" }).click();
+
+  await expect(page.locator("#logout")).toBeVisible();
+});
