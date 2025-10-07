320aa93643cb99ebf886722674bb047ed226dcdf
Ashok1328 <ashokkhatiwada1990@gmail.com>
2025-10-08 00:00:45 +0545

    Generating Allure Reports in Playwright with Retry failed test cases for the flakiness of the application

--- playwright.config.js
+++ playwright.config.js
@@ -19,7 +19,8 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+   retries: process.env.CI ? 2 : 0,
+  // retries: 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -28,9 +29,10 @@ export default defineConfig({
   // reporter: [["junit", { outputFile: "results.xml" }]],
   reporter: [
     ["list"],
-    ['html'],
+    ["html"],
     ["junit", { outputFile: "results.xml" }],
     ["json", { outputFile: "results.json" }],
+   // ["allure-playwright", { outputFolder: "my-allure-results" }],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
